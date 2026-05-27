e0bc50d4eb695784127e726f5b7cb254397ccb69
alinahib23 <ali.nahib23@gmail.com>
2026-05-27 11:50:04 +0200

    Getting started

---
+++ UnifiedMessenger/src/main/java/com/alin/messaging/unifiedmessenger/UnifiedMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.alin.messaging.unifiedmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnifiedMessengerApplication.class, args);
+    }
+
+}
---
+++ UnifiedMessenger/src/test/java/com/alin/messaging/unifiedmessenger/UnifiedMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alin.messaging.unifiedmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
